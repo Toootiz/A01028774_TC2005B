2097329dac11264b1b01ed441ac100eb737480fa
Gabriel <Toootiz.killer@gmail.com>
2024-05-26 20:55:40 -0600

    Pagina web simple

---
+++ Web/Tareas/PaginaWebSimple/app.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// Importing modules
+import express from "express";
+import fs from "fs"
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  const file = fs.readFileSync("public/html/index.html", "utf8");
+  res.status(200).send(file);
+
+})
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
+});
